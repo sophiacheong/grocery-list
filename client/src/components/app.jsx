24d1132dc77ae5581e57575ba90e5c25049c0edb
sophiacheong <sophiaacheong@gmail.com>
2021-08-11 01:34:44 -0700

    set up boiler plate for client react side

---
+++ client/src/components/app.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+const App = () => {
+  return (
+    <div>Hi</div>
+  )
+};
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const app = express();
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
