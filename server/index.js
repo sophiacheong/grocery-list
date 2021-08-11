43a8a7f089672cd91c15cd20f98fd66701146deb
sophiacheong <sophiaacheong@gmail.com>
2021-08-11 01:39:59 -0700

    added express.static to serve up html for application

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const path = require('path');
 
 const PORT = 3000;
 
@@ -8,5 +9,7 @@ const app = express();
 app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '../client/dist')))
 
 app.listen(PORT, () => console.log(`Listening at :${PORT}`))
