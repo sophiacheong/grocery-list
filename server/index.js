5df35f71f52a183e287c9538d7e70e16f1a3b496
sophiacheong <sophiaacheong@gmail.com>
2021-08-11 01:36:40 -0700

    added boiler plate for backend server

--- server/index.js
+++ server/index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 
+const PORT = 3000;
+
 const app = express();
 app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
+
+app.listen(PORT, () => console.log(`Listening at :${PORT}`))
